92947336fd0af6c9144974a9c526f29a49762e02
Andrea <mheadleybrooks@outlook.com>
2020-11-03 15:16:29 -0500

    Redux exercises

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -25,6 +25,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
        this.toggleModal();
+       this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
         console.log("current value is:" + JSON.stringify(values));
         alert('Current state is: ' + JSON.stringify(values));
     
@@ -100,7 +101,7 @@ function RenderCampsite({campsite})  {
     )
 
 }
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if(comments) {
         return (
            <div  className="col-md-5 m-1">
@@ -114,7 +115,7 @@ function RenderComments({comments}) {
 
                );
                } )};
-               <CommentForm />
+               <CommentForm campsiteId={campsiteId} addComment={addComment}/>
             </div> 
         );
       }      
@@ -146,7 +147,10 @@ function RenderComments({comments}) {
                     </div>
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id} />
                     </div>
                 </div>
             );
